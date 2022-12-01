aa518b0cb36f6088e4af326ae8546c386b399a09
Bas Apon <bas.apon@coolblue.nl>
2022-12-01 10:35:04 +0100

    Solution puzzle 1

--- puzzle_1/puzzle_1.py
+++ puzzle_1/puzzle_1.py
@@ -1,10 +1,20 @@
-import json
-
-
 def puzzle1():
     with open("input_1.txt", "r") as f:
-        data = f.read()
-    print("answer")
+        data = f.readlines()
+
+    calories = []
+    total_calories = 0
+    for row in data:
+        if row == "\n":
+            calories.append(total_calories)
+            total_calories = 0
+            continue
+        total_calories += int(row)
+
+    calories.sort()
+
+    print(f"Part 1 answer: {calories[-1:][0]}")
+    print(f"Part 2 answer: {sum(calories[-3:])}")
 
 
 if __name__ == "__main__":
